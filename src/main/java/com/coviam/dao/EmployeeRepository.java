588346de3271e336e27bcf1dbd929dfed6af4075
adityaagrawal <adityaa372@gmail.com>
2017-03-22 15:55:58 +0530

    annotate @Repository

--- src/main/java/com/coviam/dao/EmployeeRepository.java
+++ src/main/java/com/coviam/dao/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.coviam.dao;
 
 import com.coviam.model.Employee;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -9,7 +10,7 @@ import java.util.Optional;
 /**
  * Created by Aditya
  */
-
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, String> {
     Optional<Employee> findById(String id);
 }
