44c61dac703883ff7cdfe4fb0e002df2cef19c84
adityaagrawal <adityaa372@gmail.com>
2017-03-28 17:22:26 +0530

    moved configurations to config class from application.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,22 +1,13 @@
-import com.coviam.config.JpaConfig;
-import com.coviam.config.MongoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 /**
  * Created by Aditya.
  */
-@EnableCaching
-@EnableRedisRepositories(basePackages = "com.coviam.dao")
+
 @SpringBootApplication
 @ComponentScan(value = "com.coviam")
-@ContextConfiguration(classes = {JpaConfig.class, MongoConfig.class},
-        loader = AnnotationConfigContextLoader.class)
 public class Application {
     public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/coviam/config/RedisConfig.java
+++ src/main/java/com/coviam/config/RedisConfig.java
@@ -1,14 +1,18 @@
 package com.coviam.config;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 /**
  * Created by Aditya.
  */
+@EnableCaching
+@EnableRedisRepositories(basePackages = "com.coviam.dao")
 @Configuration
 public class RedisConfig {
     @Bean
